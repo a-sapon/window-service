61ba4cafc1be46c093c207ef9e00b7e07b791a61
a-sapon <sapon.anya@gmail.com>
2020-06-28 13:24:52 +0300

    Updates

---
+++ precache-manifest.3cd43c86f1e1885ddfcbfbfe1f88dcc8.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bad0d8f749e8c7eb3c74602e5cd0aa6",
+    "url": "/bud/index.html"
+  },
+  {
+    "revision": "16a7be98620d87b7725b",
+    "url": "/bud/static/css/main.4d2a8d7f.chunk.css"
+  },
+  {
+    "revision": "9586805626dfa64597f9",
+    "url": "/bud/static/js/2.13998d0b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/bud/static/js/2.13998d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a7be98620d87b7725b",
+    "url": "/bud/static/js/main.e42332f7.chunk.js"
+  },
+  {
+    "revision": "d52afc597acc579bb6dd",
+    "url": "/bud/static/js/runtime-main.0e045e52.js"
+  },
+  {
+    "revision": "58e664c3b316facf56e8f0284adb70c5",
+    "url": "/bud/static/media/background.58e664c3.jpg"
+  },
+  {
+    "revision": "41e8a965aec5e792358b08ce886c0362",
+    "url": "/bud/static/media/checkmark.41e8a965.png"
+  },
+  {
+    "revision": "55368f3723cc31a8dec1ab5973d235bc",
+    "url": "/bud/static/media/cross.55368f37.png"
+  },
+  {
+    "revision": "6f1130be9e948fca514129cf6b742c0d",
+    "url": "/bud/static/media/logo.6f1130be.png"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/bud/static/media/montserrat-v14-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/bud/static/media/montserrat-v14-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "86f6cbd269e0c5a26f038a4d5ffc9cf1",
+    "url": "/bud/static/media/montserrat-v14-latin-600.86f6cbd2.eot"
+  },
+  {
+    "revision": "a6924a5e072a58911c8588859f6825d5",
+    "url": "/bud/static/media/montserrat-v14-latin-600.a6924a5e.svg"
+  },
+  {
+    "revision": "acb878a397bf674d7baa32a3267e5a3c",
+    "url": "/bud/static/media/montserrat-v14-latin-600.acb878a3.ttf"
+  },
+  {
+    "revision": "002d0d73ef9a54b148990fa16ff2343b",
+    "url": "/bud/static/media/montserrat-v14-latin-800.002d0d73.eot"
+  },
+  {
+    "revision": "104feb7e9b5514db016426791a25b1da",
+    "url": "/bud/static/media/montserrat-v14-latin-800.104feb7e.ttf"
+  },
+  {
+    "revision": "3f167b47ccbc00d60ce08497cd1a4623",
+    "url": "/bud/static/media/montserrat-v14-latin-800.3f167b47.svg"
+  },
+  {
+    "revision": "9a9befcf50d64f9d2d19d8b1d1984add",
+    "url": "/bud/static/media/montserrat-v14-latin-800.9a9befcf.woff"
+  },
+  {
+    "revision": "b7018be9ed6cd94da8b6675b3a468c3b",
+    "url": "/bud/static/media/montserrat-v14-latin-800.b7018be9.woff2"
+  },
+  {
+    "revision": "052575c67f998b5c000924204c404cbd",
+    "url": "/bud/static/media/montserrat-v14-latin-900.052575c6.eot"
+  },
+  {
+    "revision": "26d42c9428780e545a540bbb50c84bce",
+    "url": "/bud/static/media/montserrat-v14-latin-900.26d42c94.woff"
+  },
+  {
+    "revision": "58cd789700850375b834e8b6776002eb",
+    "url": "/bud/static/media/montserrat-v14-latin-900.58cd7897.woff2"
+  },
+  {
+    "revision": "a58176a7458bdb52ef7827b782e69b88",
+    "url": "/bud/static/media/montserrat-v14-latin-900.a58176a7.svg"
+  },
+  {
+    "revision": "c2640c21891450340d225c504e9f74c0",
+    "url": "/bud/static/media/montserrat-v14-latin-900.c2640c21.ttf"
+  },
+  {
+    "revision": "5cc74ef8a4c422084726eb9dd1163b82",
+    "url": "/bud/static/media/montserrat-v14-latin-regular.5cc74ef8.eot"
+  },
+  {
+    "revision": "6a9e85ac9247f5848db957b873c62e0c",
+    "url": "/bud/static/media/montserrat-v14-latin-regular.6a9e85ac.ttf"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/bud/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/bud/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "f3fef7e587e6c436df1d7985b2a90ada",
+    "url": "/bud/static/media/montserrat-v14-latin-regular.f3fef7e5.svg"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/bud/static/media/telegram.a888fec0.svg"
+  },
+  {
+    "revision": "0528958450745f2b7397b95f0871120f",
+    "url": "/bud/static/media/viber.05289584.svg"
+  }
+]);
