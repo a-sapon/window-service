7d014a7e2ee34852548933145055c3ad11b6f02a
a-sapon <sapon.anya@gmail.com>
2020-07-10 13:38:09 +0300

    Updates

---
+++ precache-manifest.b63d243d5c20ee2de6031901b643947c.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c47644b2a7f7abb5642929346c759820",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df9f2a0b35cda79ccefd",
+    "url": "/static/css/main.3de2cf2c.chunk.css"
+  },
+  {
+    "revision": "7e61313cc324d8ff3c26",
+    "url": "/static/js/2.6f7c97a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6f7c97a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df9f2a0b35cda79ccefd",
+    "url": "/static/js/main.5d8790d6.chunk.js"
+  },
+  {
+    "revision": "f1a93adb81457057092e",
+    "url": "/static/js/runtime-main.b49a8921.js"
+  },
+  {
+    "revision": "1bdbf48b814c581522c92a7e07c07f6f",
+    "url": "/static/media/background.1bdbf48b.jpg"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/static/media/montserrat-v14-latin-600.6fb1b562.woff2"
+  },
+  {
+    "revision": "7c839d15a6f54e7025ba8c0c4b333e8f",
+    "url": "/static/media/montserrat-v14-latin-600.7c839d15.woff"
+  },
+  {
+    "revision": "86f6cbd269e0c5a26f038a4d5ffc9cf1",
+    "url": "/static/media/montserrat-v14-latin-600.86f6cbd2.eot"
+  },
+  {
+    "revision": "a6924a5e072a58911c8588859f6825d5",
+    "url": "/static/media/montserrat-v14-latin-600.a6924a5e.svg"
+  },
+  {
+    "revision": "acb878a397bf674d7baa32a3267e5a3c",
+    "url": "/static/media/montserrat-v14-latin-600.acb878a3.ttf"
+  },
+  {
+    "revision": "002d0d73ef9a54b148990fa16ff2343b",
+    "url": "/static/media/montserrat-v14-latin-800.002d0d73.eot"
+  },
+  {
+    "revision": "104feb7e9b5514db016426791a25b1da",
+    "url": "/static/media/montserrat-v14-latin-800.104feb7e.ttf"
+  },
+  {
+    "revision": "3f167b47ccbc00d60ce08497cd1a4623",
+    "url": "/static/media/montserrat-v14-latin-800.3f167b47.svg"
+  },
+  {
+    "revision": "9a9befcf50d64f9d2d19d8b1d1984add",
+    "url": "/static/media/montserrat-v14-latin-800.9a9befcf.woff"
+  },
+  {
+    "revision": "b7018be9ed6cd94da8b6675b3a468c3b",
+    "url": "/static/media/montserrat-v14-latin-800.b7018be9.woff2"
+  },
+  {
+    "revision": "052575c67f998b5c000924204c404cbd",
+    "url": "/static/media/montserrat-v14-latin-900.052575c6.eot"
+  },
+  {
+    "revision": "26d42c9428780e545a540bbb50c84bce",
+    "url": "/static/media/montserrat-v14-latin-900.26d42c94.woff"
+  },
+  {
+    "revision": "58cd789700850375b834e8b6776002eb",
+    "url": "/static/media/montserrat-v14-latin-900.58cd7897.woff2"
+  },
+  {
+    "revision": "a58176a7458bdb52ef7827b782e69b88",
+    "url": "/static/media/montserrat-v14-latin-900.a58176a7.svg"
+  },
+  {
+    "revision": "c2640c21891450340d225c504e9f74c0",
+    "url": "/static/media/montserrat-v14-latin-900.c2640c21.ttf"
+  },
+  {
+    "revision": "5cc74ef8a4c422084726eb9dd1163b82",
+    "url": "/static/media/montserrat-v14-latin-regular.5cc74ef8.eot"
+  },
+  {
+    "revision": "6a9e85ac9247f5848db957b873c62e0c",
+    "url": "/static/media/montserrat-v14-latin-regular.6a9e85ac.ttf"
+  },
+  {
+    "revision": "8102c4838f9e3d08dad644290a9cb701",
+    "url": "/static/media/montserrat-v14-latin-regular.8102c483.woff"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/static/media/montserrat-v14-latin-regular.bc3aa95d.woff2"
+  },
+  {
+    "revision": "f3fef7e587e6c436df1d7985b2a90ada",
+    "url": "/static/media/montserrat-v14-latin-regular.f3fef7e5.svg"
+  },
+  {
+    "revision": "a888fec0a2859e59674a89bc7722e062",
+    "url": "/static/media/telegram.a888fec0.svg"
+  },
+  {
+    "revision": "0528958450745f2b7397b95f0871120f",
+    "url": "/static/media/viber.05289584.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/bud/precache-manifest.0492a74320bfe569da5d913694faf385.js"
+  "/precache-manifest.b63d243d5c20ee2de6031901b643947c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/bud/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
